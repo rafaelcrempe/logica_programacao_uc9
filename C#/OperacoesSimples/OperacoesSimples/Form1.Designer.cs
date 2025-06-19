d4f6e51315c9e5b5983708300fd7109c6bb533ad
Rafael Crempe <rafaelcrempe@gmail.com>
2025-06-19 13:33:57 -0300

    início das atividades em C#

---
+++ C#/OperacoesSimples/OperacoesSimples/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace OperacoesSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAdicao = new System.Windows.Forms.Label();
+            this.lblSubtracao = new System.Windows.Forms.Label();
+            this.lblMultiplicacao = new System.Windows.Forms.Label();
+            this.lblDivisao = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero1.Location = new System.Drawing.Point(69, 57);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(177, 29);
+            this.txtNumero1.TabIndex = 0;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero2.Location = new System.Drawing.Point(69, 139);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(177, 29);
+            this.txtNumero2.TabIndex = 1;
+            this.txtNumero2.TextChanged += new System.EventHandler(this.txtNumero2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite um número:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Digite outro número:";
+            // 
+            // lblAdicao
+            // 
+            this.lblAdicao.AutoSize = true;
+            this.lblAdicao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdicao.Location = new System.Drawing.Point(22, 295);
+            this.lblAdicao.Name = "lblAdicao";
+            this.lblAdicao.Size = new System.Drawing.Size(0, 24);
+            this.lblAdicao.TabIndex = 4;
+            // 
+            // lblSubtracao
+            // 
+            this.lblSubtracao.AutoSize = true;
+            this.lblSubtracao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtracao.Location = new System.Drawing.Point(22, 326);
+            this.lblSubtracao.Name = "lblSubtracao";
+            this.lblSubtracao.Size = new System.Drawing.Size(0, 24);
+            this.lblSubtracao.TabIndex = 5;
+            this.lblSubtracao.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblMultiplicacao
+            // 
+            this.lblMultiplicacao.AutoSize = true;
+            this.lblMultiplicacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiplicacao.Location = new System.Drawing.Point(22, 357);
+            this.lblMultiplicacao.Name = "lblMultiplicacao";
+            this.lblMultiplicacao.Size = new System.Drawing.Size(0, 24);
+            this.lblMultiplicacao.TabIndex = 6;
+            // 
+            // lblDivisao
+            // 
+            this.lblDivisao.AutoSize = true;
+            this.lblDivisao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDivisao.Location = new System.Drawing.Point(22, 386);
+            this.lblDivisao.Name = "lblDivisao";
+            this.lblDivisao.Size = new System.Drawing.Size(0, 24);
+            this.lblDivisao.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(93, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(304, 465);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblDivisao);
+            this.Controls.Add(this.lblMultiplicacao);
+            this.Controls.Add(this.lblSubtracao);
+            this.Controls.Add(this.lblAdicao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAdicao;
+        private System.Windows.Forms.Label lblSubtracao;
+        private System.Windows.Forms.Label lblMultiplicacao;
+        private System.Windows.Forms.Label lblDivisao;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ C#/OperacoesSimples/OperacoesSimples/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OperacoesSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal num1 = Convert.ToDecimal(txtNumero1.Text);
+            decimal num2 = Convert.ToDecimal(txtNumero2.Text);
+            decimal adicao = num1 + num2;
+            decimal subtracao = num1 - num2;
+            decimal divisao = num1 / num2;
+            decimal multiplicacao = num1 * num2;
+            lblAdicao.Text = $"SOMA: {num1} + {num2} = {adicao.ToString()}.";
+            lblSubtracao.Text = $"SUBTRAÇÃO: {num1} - {num2} = {subtracao.ToString()}.";
+            lblMultiplicacao.Text = $"MULTIPLICAÇÃO: {num1} * {num2} = {multiplicacao.ToString()}.";
+            lblDivisao.Text = $"DIVISÃO: {num1} / {num2} = {divisao.ToString()}.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
