98bd5b39ab4bbd1ee122e8b58dd9dab73825e347
Rafael Crempe <rafaelcrempe@gmail.com>
2025-08-08 17:21:51 -0300

    atividade final

--- JavaScript/atividade_final.js
+++ JavaScript/atividade_final.js
@@ -1,3 +1,12 @@
+
+/* O sistema foi criado para administrar o setor de farmácia de uma UPA (Unidade de Pronto Atendimento). Nele você pode listar e 
+cadastrar novos medicamentos, além de alterar a quantidade de medicamentos disponíveis na unidade. 
+Por padrão, a senha de todos os usuários é 123456, mas nem todos possuem permissão para acessar esse setor.
+Os usuários disponíveis são Rose, Rafael, Conrado e Eduardo*/
+
+
+
+
 let prompt = require("prompt-sync")()
 
 let usuarios = [
@@ -75,9 +84,9 @@ function principalFarmacia() {
         listarMedicamentos()
         return
     } else if (opcao == "2") {
-        cadastrar()
+        cadastrarMedicamento()
     } else if (opcao == "3") {
-        return
+        main()
     } else {
         console.log("Opção inválida!")
         setTimeout(() => {
@@ -92,14 +101,103 @@ function listarMedicamentos() {
     for (let i = 0; i < medicamentos.length; i++) {
         console.log(`ID: [${medicamentos[i].id}] | Nome: ${medicamentos[i].nome} | Tipo: ${medicamentos[i].tipo} | Quantidade: ${medicamentos[i].quantidade}`)
     }
-
-    let alteracao = prompt("Deseja 1) CADASTAR MEDICAMENTO ou 2) ALTERAR MEDICAMENTO?")
+    
+    let alteracao = prompt("\nDeseja 1) CADASTAR MEDICAMENTO, 2) ALTERAR QUANTIDADE ou 3) SAIR? ")
 
     if (alteracao == "1") {
         console.clear()
         cadastrarMedicamento()
-    } else if (opcao == "2") {
+    } else if (alteracao == "2") {
         alterarMedicamento()
+
+    } else if (alteracao == "3") {
+        console.clear()
+        principalFarmacia()
+    } else {
+        console.clear()
+        console.log("Opção inválida!")
+        setTimeout(() => {
+            listarMedicamentos()            
+        }, 1500);
     }
 
+}
+
+function cadastrarMedicamento() {
+    console.clear()
+    
+    let nome = prompt("Digite o nome do medicamento: ").toLowerCase()
+
+    if (nome == "") {
+        console.log("O nome não pode ficar em branco!")
+        prompt("Aperte ENTER para retornar")
+        cadastrarMedicamento()
+    }
+
+
+    let cadastrado = false
+    
+    for (let i = 0; i < medicamentos.length; i++) {
+        if (medicamentos[i].nome.toLowerCase() === nome.toLowerCase()) {
+            cadastrado = true
+            console.clear()
+            console.log("Medicamento já cadastrado")
+            setTimeout(() => {
+                principalFarmacia()
+            }, 1500);
+            return
+        }
+
+    }
+
+    let tipo = prompt("Digite o tipo do medicamento: ").toLowerCase()
+   if (tipo == "") {
+        console.log("O tipo não pode ficar em branco!")
+        prompt("Aperte ENTER para retornar")
+        cadastrarMedicamento()
+    }
+
+    let quantidade = parseInt(prompt("Digite a quantidade disponível do medicamento: "))
+
+    if (quantidade <= 0) {
+        console.log("Não é possível cadastrar um medicamento indisponível")
+        setTimeout(() => {
+            principalFarmacia()            
+        }, 1500);
+        return
+
+    }
+
+    medicamentos.push({ nome: nome, tipo: tipo, quantidade: quantidade, id: medicamentos.length + 1 })
+
+    console.log("Medicamento cadastrado com sucesso!")
+
+    setTimeout(() => {
+        listarMedicamentos()
+    }, 1500);
+
+
+}
+
+function alterarMedicamento() {
+
+    let posicao = prompt("Digite o ID do medicamento que deseja alterar: ")
+
+    console.clear()
+
+    if (medicamentos.length >= posicao) {
+        console.log(`Medicamento selecionado: ${medicamentos[posicao - 1].nome}`)
+        alterar(posicao - 1)
+    } else {
+        console.log("Não existe um medicamento cadastrado nessa posição!")
+    }
+
+    function alterar(indice) {
+        let nova_quantidade = prompt("Digite a nova quantidade de medicamento: ")
+        medicamentos[indice].quantidade = nova_quantidade
+        console.log("Quantidade alterada com sucesso!")
+        setTimeout(() => {
+            principalFarmacia()
+        }, 1500);
+    }
 }
